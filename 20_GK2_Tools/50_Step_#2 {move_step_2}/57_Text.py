9b4daabaa71b51f85d7fa9d96749f8784d02ac8b
Minichev Alexandr <139385189+panasonique@users.noreply.github.com>
2026-02-26 14:21:07 +0100

    Delete 20_GK2_Tools directory

--- 20_GK2_Tools/50_Step_#2 {move_step_2}/57_Text.py
+++
@@ -1,6 +0,0 @@
-info = {
-    "name": "Align to Grid (Step #2)",
-    "type": "text",
-    "align": "LEFT",  # Доступно: 'LEFT', 'CENTER', 'RIGHT'
-    "wrap": 50  # Максимальное количество символов в одной строке
-}
--- 20_GK2_Tools/60_Switch_Surfaces/75_Text.py
+++
@@ -1,6 +0,0 @@
-info = {
-    "name": "Debug Vertex Colors",
-    "type": "text",
-    "align": "LEFT",  # Доступно: 'LEFT', 'CENTER', 'RIGHT'
-    "wrap": 50  # Максимальное количество символов в одной строке
-}
