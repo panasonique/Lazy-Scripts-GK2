1a90b045ae71ec88680b0a55514dd49015d9cb08
Minichev Alexandr <139385189+panasonique@users.noreply.github.com>
2026-02-26 12:48:12 +0100

    Update Template for Button.py

--- Template for Button.py
+++ Template for Button.py
@@ -3,15 +3,21 @@ info = {
     "icon": "CUBE",        # Иконка из Blender API
     "description": "Описание при наведении",
     "type": "button",      # Тип по умолчанию
+    "prop_id": "int_value",
     "shortcut": "Alt Shift X"
 }
 
 import bpy
+# Динамическое получение значения: 
+# Мы смотрим в info["prop_id"], получаем строку "int_value" 
+# и ищем поле с таким именем внутри объекта props
+var = getattr(props, info.get("prop_id"), 0.0)
 
 def run():
     # ВАШ КОД ЗДЕСЬ
-    print("Скрипт выполнен")
+    print(f"Скрипт выполнен. Значение int_value: {var}")
     # Пример: report({'INFO'}, "Готово!") — если проброшен report
 
 if __name__ == "__main__":
     run()
+
