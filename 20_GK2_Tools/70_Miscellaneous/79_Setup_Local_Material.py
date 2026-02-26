a12cd16a83fc862f18185ff4997167b054e398f0
Minichev Alexandr <139385189+panasonique@users.noreply.github.com>
2026-02-26 17:46:10 +0100

    Update 79_Setup_Local_Material.py
    поиск по всем # папкам.

--- 20_GK2_Tools/70_Miscellaneous/79_Setup_Local_Material.py
+++ 20_GK2_Tools/70_Miscellaneous/79_Setup_Local_Material.py
@@ -1,7 +1,7 @@
 info = {
     "name": "Init Material",
-    "description": "Создает локальный материал и синхронизирует текстуру с GN модификатором",
-    "icon": "MATERIAL",
+    "description": "Поиск текстур в любых папках '#' и синхронизация с GN",
+    "icon": 'MATERIAL',
     "type": "button",
     "shortcut": ""
 }
@@ -9,22 +9,35 @@ info = {
 import os
 import bpy
 
+def find_texture_recursive(base_path, filename):
+    """Рекурсивно ищет файл в папках, начинающихся с #"""
+    for root, dirs, files in os.walk(base_path):
+        # Оставляем для обхода только те папки, которые начинаются с #
+        # (Проверяем только текущий уровень dirs, чтобы os.walk шел дальше)
+        dirs[:] = [d for d in dirs if d.startswith("#")]
+        
+        if filename in files:
+            return os.path.join(root, filename)
+    return None
+
 def run():
-    # 1. Проверяем, сохранен ли .blend файл
     if not bpy.data.is_saved:
-        print("Ошибка: Сначала сохраните .blend файл!")
+        report({'ERROR'}, "Сначала сохраните .blend файл!")
         return
 
-    obj = bpy.context.active_object
+    obj = context.active_object # Используем проброшенный context из аддона
     source_mat = bpy.data.materials.get("#Default Material")
 
-    if not (obj and obj.type == 'MESH' and source_mat):
-        if not source_mat: print("Ошибка: '#Default Material' не найден.")
+    if not (obj and obj.type == 'MESH'):
+        return
+    
+    if not source_mat:
+        report({'ERROR'}, "'#Default Material' не найден.")
         return
 
     target_name = obj.name
     
-    # 2. Пересоздание материала
+    # --- 1. Работа с материалом ---
     old_mat = bpy.data.materials.get(target_name)
     if old_mat:
         bpy.data.materials.remove(old_mat, do_unlink=True)
@@ -37,11 +50,10 @@ def run():
     else:
         obj.data.materials.append(new_mat)
 
-    # 3. Работа с текстурой
+    # --- 2. Поиск текстуры (Новая логика) ---
     project_dir = os.path.dirname(bpy.data.filepath)
-    textures_dir = os.path.join(project_dir, "#Textures")
     img_name = f"{target_name}.png"
-    img_path = os.path.join(textures_dir, img_name)
+    img_path = find_texture_recursive(project_dir, img_name)
 
     new_mat.use_nodes = True
     nodes = new_mat.node_tree.nodes
@@ -56,7 +68,8 @@ def run():
             links.new(tex_node.outputs['Color'], bsdf.inputs['Base Color'])
 
     loaded_image = None
-    if os.path.exists(img_path):
+    if img_path:
+        # Очистка старой версии из памяти Blender
         old_img = bpy.data.images.get(img_name)
         if old_img:
             bpy.data.images.remove(old_img, do_unlink=True)
@@ -65,60 +78,39 @@ def run():
             loaded_image = bpy.data.images.load(img_path)
             tex_node.image = loaded_image
             loaded_image.reload()
+            print(f"Найдено: {img_path}")
         except Exception as e:
             print(f"Ошибка загрузки: {e}")
+    else:
+        print(f"Файл {img_name} не найден ни в одной папке #...")
 
-    # --- 4. СИНХРОНИЗАЦИЯ С GEOMETRY NODES (Метод прямого перебора ID-блоков) ---
+    # --- 3. Синхронизация с Geometry Nodes ---
     gn_mod = next((m for m in obj.modifiers if m.type == 'NODES' and m.node_group), None)
     
-    if gn_mod:
-        print(f"--- Синхронизация с {gn_mod.name} ---")
-        
-        # 1. Подготовка картинки (как кнопка Open/New)
-        img_name = f"{obj.name}.png"
-        image = bpy.data.images.get(img_name)
+    if gn_mod and (loaded_image or img_path):
+        # Если картинка не была загружена выше (не было файла), создаем пустую
+        image = loaded_image
         if not image:
-            project_dir = os.path.dirname(bpy.data.filepath)
-            img_path = os.path.join(project_dir, "#Textures", img_name)
-            image = bpy.data.images.load(img_path) if os.path.exists(img_path) else bpy.data.images.new(img_name, 1024, 1024)
+            image = bpy.data.images.new(img_name, 1024, 1024)
 
-        # 2. ПОИСК ПО "СКРЫТЫМ" КЛЮЧАМ (Обход всех ошибок API 4.x)
-        # Мы ищем идентификатор сокета, который называется 'Texture Image'
         target_id = None
-        
-        # Сначала пробуем найти идентификатор через саму нодовую группу (через дерево)
-        # В 4.x у дерева есть входы, доступные через .inputs (в некоторых сборках еще осталось)
-        # или через прямое перечисление ID-данных
         try:
-            # Прямой перебор структуры сокетов (без слова items)
-            for i in range(len(gn_mod.node_group.interface.items_tree)):
-                item = gn_mod.node_group.interface.items_tree[i]
+            # Поиск идентификатора сокета в 4.x
+            for item in gn_mod.node_group.interface.items_tree:
                 if item.name == "Texture Image":
                     target_id = item.identifier
                     break
         except:
-            # Если дерево заблокировано намертво, ищем по именам в самом модификаторе
             for key in gn_mod.keys():
-                if "Texture Image" in key or "Input" in key:
+                if "Texture Image" in key:
                     target_id = key
                     break
 
-        # 3. ПРИНУДИТЕЛЬНАЯ ЗАПИСЬ (Аналог выбора из списка)
-        if target_id and image:
-            # Запись по системному ключу инициализирует поле в "свежем" модификаторе
+        if target_id:
             gn_mod[target_id] = image
             if tex_node:
                 tex_node.image = image
-            print(f"УСПЕХ: Картинка '{image.name}' назначена в сокет '{target_id}'")
-        else:
-            # Если поле СОВСЕМ не найдено (модификатор только что добавлен и пуст)
-            # Мы пробуем записать в самый первый подходящий по типу слот 'Socket_1' и т.д.
-            print("ВНИМАНИЕ: Поле не найдено. Попробуйте вручную один раз выбрать картинку в GN.")
 
-    bpy.context.area.tag_redraw()
-
-
-
-    bpy.context.area.tag_redraw()
+    context.area.tag_redraw()
 
 run()
